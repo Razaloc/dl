6dfc79d4891eb32e1bc323d509ab3797cfe87373
Rafael Garcia Ruiz <rafael.garcia@collabora.com>
2022-10-09 19:22:04 +0200

    Add a range request download method
    Prepare the download by range request, add test but still miss the flag
    implementation.
    In this case the body is divided in ranges and sent in parts.
    
    Signed-off-by: Rafael Garcia Ruiz <rafael.garcia@collabora.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,56 @@
 mod tests;
+use error_chain::error_chain;
 use hyper::body::HttpBody;
-use hyper::{Client, Uri};
+use hyper::header::{CONTENT_LENGTH, RANGE};
+use hyper::http::HeaderValue;
+use hyper::{Client, StatusCode, Uri};
 use hyper_tls::HttpsConnector;
+use std::fs::File;
+use std::str::FromStr;
 use std::{env, io::Write};
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        Reqwest(reqwest::Error);
+        Header(reqwest::header::ToStrError);
+    }
+}
+struct PartialRangeIter {
+    start: u64,
+    end: u64,
+    buffer_size: u32,
+}
+
+impl PartialRangeIter {
+    pub fn new(start: u64, end: u64, buffer_size: u32) -> Result<Self> {
+        if buffer_size == 0 {
+            Err("invalid buffer_size, give a value greater than zero.")?;
+        }
+        Ok(PartialRangeIter {
+            start,
+            end,
+            buffer_size,
+        })
+    }
+}
+
+impl Iterator for PartialRangeIter {
+    type Item = HeaderValue;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start > self.end {
+            None
+        } else {
+            let prev_start = self.start;
+            self.start += std::cmp::min(self.buffer_size as u64, self.end - self.start + 1);
+            Some(
+                HeaderValue::from_str(&format!("bytes={}-{}", prev_start, self.start - 1))
+                    .expect("string provided by format!"),
+            )
+        }
+    }
+}
+type Result1<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 pub fn setup(element: &str) -> (Uri, &str) {
     let fpath = element.split('/').last().expect("Fail to find file name");
@@ -16,7 +62,7 @@ pub fn setup(element: &str) -> (Uri, &str) {
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> Result1<()> {
     //Setting up argument as filename and url
     let args: Vec<String> = env::args().collect();
     if args.len() <= 1 {
@@ -38,7 +84,7 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-pub async fn fetch_url_http(url: hyper::Uri, fpath: &str) -> Result<()> {
+pub async fn fetch_url_http(url: hyper::Uri, fpath: &str) -> Result1<()> {
     let client = Client::new();
     let mut res = client.get(url).await?;
     let mut file = std::fs::File::create(fpath).unwrap();
@@ -53,7 +99,7 @@ pub async fn fetch_url_http(url: hyper::Uri, fpath: &str) -> Result<()> {
     Ok(())
 }
 
-pub async fn fetch_url_https(url: hyper::Uri, fpath: &str) -> Result<()> {
+pub async fn fetch_url_https(url: hyper::Uri, fpath: &str) -> Result1<()> {
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
 
@@ -67,5 +113,37 @@ pub async fn fetch_url_https(url: hyper::Uri, fpath: &str) -> Result<()> {
 
     println!("\n\nDone!");
 
+    Ok(())
+}
+
+pub async fn fetch_url_http_range_requests(url: hyper::Uri, fpath: &str) -> Result<()> {
+    const CHUNK_SIZE: u32 = 10240;
+    let client = reqwest::Client::new();
+    let url = url.to_string();
+    let response = client.head(&url).send().await?;
+    let length = response
+        .headers()
+        .get(CONTENT_LENGTH)
+        .ok_or("response doesn't include the content length")?;
+    let length = u64::from_str(length.to_str()?).map_err(|_| "invalid Content-Length header")?;
+
+    let mut output_file = File::create(fpath)?;
+
+    println!("starting download...");
+    for range in PartialRangeIter::new(0, length - 1, CHUNK_SIZE)? {
+        println!("range {:?}", range);
+        let inner_response = client.get(&url).header(RANGE, range).send().await?;
+
+        println!("------------> {:?} ", inner_response.content_length());
+        let status = inner_response.status();
+        if !(status == StatusCode::OK || status == StatusCode::PARTIAL_CONTENT) {
+            error_chain::bail!("Unexpected server response: {}", status)
+        }
+        let content = &inner_response.bytes().await?;
+        output_file.write_all(content)?;
+    }
+    let content = response.bytes().await?;
+    output_file.write_all(&content)?;
+    println!("Finished with success!");
     Ok(())
 }
--- src/tests.rs
+++ src/tests.rs
@@ -2,7 +2,7 @@
 use super::*;
 
 #[tokio::test]
-async fn test_http() -> Result<()> {
+async fn test_http() -> Result1<()> {
     let element = "http://adaptive-images.com/content/images/winter.jpg".to_string();
     let (url, fpath) = setup(&element);
     fetch_url_http(url, fpath).await?;
@@ -16,10 +16,24 @@ async fn test_http() -> Result<()> {
 }
 
 #[tokio::test]
-async fn test_https() -> Result<()> {
+async fn test_https() -> Result1<()> {
     let element = "https://www.rust-lang.org/logos/rust-logo-128x128-blk.png".to_string();
     let (url, fpath) = setup(&element);
     fetch_url_https(url, fpath).await?;
+    let mut file1 = std::fs::File::open(fpath)?;
+    let mut file2 = std::fs::File::open("./tests/data/".to_string() + fpath)?;
+    match file_diff::diff_files(&mut file1, &mut file2) {
+        true => std::fs::remove_file(fpath).expect("File delete failed"),
+        false => panic!("The downloaded file aint the same as the tests/data/ one"),
+    };
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_http_range_req() -> Result<()> {
+    let element = "http://adaptive-images.com/content/images/winter.jpg".to_string();
+    let (url, fpath) = setup(&element);
+    fetch_url_http_range_requests(url, fpath).await?;
     let mut file1 = std::fs::File::open(fpath)?;
     let mut file2 = std::fs::File::open("./tests/data/".to_string() + fpath)?;
     match file_diff::diff_files(&mut file1, &mut file2) {
